f66435f40fe3b07958f54959e36cee6baa6ca13a
Christopher Ksiazak <cksiazak@gmail.com>
2019-08-23 19:47:04 -0600

    added styles and fit the nav bar for the user card

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -6,18 +6,29 @@ import Logo from './Styles/Logo.png';
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <img src={Logo} alt="SIXR Logo" />
-      <div>
+      <img src={Logo} alt="SIXR Logo" className="sixr-logo" />
+      <div className="nav-wrapper">
         <NavLink exact={true} to="/">
           Home
         </NavLink>
         <NavLink exact={true} activeClassName="active" to="/projects">
           Projects
         </NavLink>
-        
+
         {/* //IF user is logged in, show below */}
-  <NavLink exact={true} activeClassName="active" to="/projects">Jeff the philanthropic one</NavLink>
-        
+        <div className="user-navcard">
+          <NavLink exact={true} activeClassName="active" to="/user" className="user-card-link">
+            Jeff the philanthropic one
+            <div className="img-wrapper">
+              <img
+                src="https://i.imgur.com/j8xvz88.jpg"
+                alt="user-icon"
+                className="user-icon"
+              />
+            </div>
+          </NavLink>
+        </div>
+
         {/* //UNLESS user is not logged in, display below */}
 
         {/* <NavLink exact={true} activeClassName="active" to="/login">
@@ -31,7 +42,6 @@ const NavBar = () => {
         >
           Sign Up
         </NavLink> */}
-       
       </div>
     </nav>
   );
