c9f878e0dc098fd7060a91ea70c8e55c17b0a73f
Christopher Ksiazak <cksiazak@gmail.com>
2019-08-25 01:20:11 -0600

    added yup validation

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import './Styles/login.scss';
 
-const Login = () => {
+const Login = ({ errors, touched }) => {
   return (
     <section className="login-form">
       <div className="login-container">
@@ -10,11 +11,13 @@ const Login = () => {
         <Form>
           <div className="form-element">
             <h2>EMAIL</h2>
-            <Field type="email" name="email" />
+            <Field type="email" name="email" spellCheck="false" />
+            {touched.email && errors.email && <p>{errors.email}</p>}
           </div>
           <div className="form-element">
             <h2>PASSWORD</h2>
             <Field type="password" name="password" />
+            {touched.password && errors.password && <p>{errors.password}</p>}
           </div>
           <div className="forgot-password">
             <a href="#">Forgot Password</a>
@@ -32,6 +35,21 @@ const FomikLoginForm = withFormik({
       email: email || '',
       password: password || ''
     };
+  },
+
+  //=======Validation Schema======
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email('Email is not valid')
+      .required('Email is not valid'),
+    password: Yup.string()
+      .min(6, 'Password must be 6 characters or longer')
+      .required('Password is required')
+  }),
+  //=====End Validation Schema====
+
+  handleSubmit(values) {
+    console.log(values);
   }
 })(Login);
 
