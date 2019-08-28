8c9ad62803ce264263743336f1713c0f225f300f
thaveesit douangaphaivong <d15.landon@gmail.com>
2019-08-27 21:30:47 -0500

    Footer Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p>
+        
+      </p>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   );
 }
 
+
 export default App;
+
---
+++ client/src/Components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {IoLogoFacebook} from 'react-icons/io';
+import {IoLogoTwitter} from 'react-icons/io';
+import {IoLogoYoutube} from "react-icons/io";
+import './Footer.css';
+
+
+
+
+
+
+function Footer () {
+    return (
+    
+    <footer>
+            <img src="https://files.slack.com/files-pri/T4JUEB3ME-FMSTV7THV/image.png" alt="VR Headgear"></img>
+            <div class="footer-social">
+                <p>&copy;SIXER 2019 - Simulated Immersive eXperimental Realities | Brooklyn Ave NE, Seattle, WA, 98105 | 206-571-8430 | SIXR@Studivo.com</p>
+                <p>Social Media Links</p>
+                <div className="sml">
+                 <a class="fab fa-facebook" href="https://www.facebook.com/sixrvr ">
+                    <IoLogoFacebook className="facebook" />
+                    </a>
+                    <a class="fab fa-twitter" href="https://twitter.com/sixrvr">
+                    <IoLogoTwitter className="twitter" />
+                    </a>
+                    <a class="fab fa-youtube"href="https://www.youtube.com/channel/UCVavPlD8iCYHyKhVJ06PAlw/featured ">
+                    <IoLogoYoutube className="youtube" />
+                    </a>
+                 </div>
+            </div>
+  </footer>
+    
+    )
+};
+
+export default Footer;
