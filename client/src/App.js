94ca49a5fb3bfdf178114e20f5b139e80f0d7a4a
thaveesit douangaphaivong <d15.landon@gmail.com>
2019-08-30 20:59:49 -0500

    Compared

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect, Link } from 'react-router-dom';
+import { useMedia } from 'react-use';
 import NavBar from './Components/NavBar';
+import NavBarUser from './Components/NavBarUser';
+import NavBarMobile from './Components/NavBar-mobile';
+import NavBarUserMobile from './Components/NavBarUser-mobile';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
 import ProjectList from './Components/ProjectList';
-import axios from 'axios';
-import './App.css';
 import Footer from "./Components/Footer";
-import DonorCard from './Components/DonorCard';
+import DonorList from "./Components/DonorList"
+import axios from 'axios';
+
+import Protected from './Components/Protected';
+import BGImg from './Components/BG-Img';
+import './App.css';
 
 
 function App() {
-  const [projects, setProjects] = useState();
+  //this determines whether the navbar shows the user info or not
+  const [navUser, setNavUser] = useState(false);
+  const match800 = useMedia('(max-width: 800px)');
 
+  //Fake authorization
+  const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+      this.isAuthenticated = true;
+      setNavUser(true);
+      setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+      this.isAuthenticated = false;
+      setNavUser(false);
+      setTimeout(cb, 100); // fake async
+    }
+  };
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        fakeAuth.isAuthenticated === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+
+  const [projects, setProjects] = useState();
   useEffect(() => {
     axios
       .get('https://vr-overlord-server.herokuapp.com/projects/')
@@ -26,18 +64,28 @@ function App() {
 
   return (
     <div className="App">
-     <NavBar />
-     <DonorCard/>
-      {/* <Route path="/projects" component={Projects} /> */}
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
+      <BGImg />
+        {match800
+          ? [navUser ? <NavBarUserMobile /> : <NavBarMobile />]
+          : [navUser ? <NavBarUser /> : <NavBar />]}
+        <Link to="/protected">Protected</Link>
+        <Route
+          exact
+          path="/"
+          render={props => <Login {...props} auth={fakeAuth} />}
+        />
+        <Route
+          path="/signup"
+          render={props => <Signup {...props} auth={fakeAuth} />}
+        />
+        <PrivateRoute path="/protected" component={Protected} />
       <Route path="/projects" render={(props) => {
         return projects && <ProjectList {...props} projects={projects} />;
       }} />
+      <Route path="/user" render={(props) => {
+        return projects && <DonorList  {...props} projects={projects} />;
+      }}/>
       <Footer/>
-      {/* <footer>
-        <Footer/>
-      </footer> */}
     </div>
   );
 }
